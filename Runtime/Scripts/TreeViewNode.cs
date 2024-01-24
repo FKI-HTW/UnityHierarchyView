e8041b523924e13187846a09de048b676002077c
jKnepel <knepel.julian@tutanota.com>
2024-01-24 14:51:58 +0100

    chore: cleanup, remove configuration, add onFolded callback to node

--- Runtime/Scripts/TreeViewNode.cs
+++ Runtime/Scripts/TreeViewNode.cs
@@ -6,105 +6,66 @@ namespace VENTUS.UnityHierarchyView
 {
 	public class TreeViewNode : IDisposable
 	{
-        public UINode UIInstance { get; private set; }
+		#region fields
 
         private readonly HierarchyManager mMgrRef;
         private readonly TreeViewNode mParentRef;
         private readonly Transform mTransform;
-
         private readonly List<TreeViewNode> mChildren;
-        private bool mUnfold = true;
-        private int mRowIndex = 0;
+        private readonly int mDepth;
+
+		private UINode mUIInstance;
+        
+		private bool mUnfold = true;
 		private bool disposedValue;
 
-		private const int mBtnSize = 25;
+		#endregion
 
-        public TreeViewNode(HierarchyManager manager, Transform transform)
+		#region lifecycle
+
+		public TreeViewNode(HierarchyManager manager, Transform transform)
         {
             mMgrRef = manager;
             mParentRef = null;
             mTransform = transform;
-            mRowIndex = 0;
             mChildren = new();
+            mDepth = 0;
 
-			InititializeNode(manager);
-            UpdateLocation(mRowIndex);
-
+			InitializeNode();
             InitializeChildren();
 		}
 
-        private TreeViewNode(HierarchyManager manager, Transform transform, TreeViewNode parent, int index)
+        private TreeViewNode(HierarchyManager manager, Transform transform, TreeViewNode parent, int depth)
         {
             mMgrRef = manager;
 			mParentRef = parent;
             mTransform = transform;
-			mRowIndex = index;
 			mChildren = new();
+			mDepth = depth;
 
-			InititializeNode(manager);
-			UpdateLocation(mRowIndex);
-
+			InitializeNode();
             InitializeChildren();
 		}
 
-        private void InitializeChildren()
-        {
-			int childCount = mTransform.childCount;
-			for (int i = 0; i < childCount; i++)
-			{
-				TreeViewNode child = new(mMgrRef, mTransform.GetChild(i), this, mRowIndex + 1);
-				mChildren.Add(child);
-			}
-		}
-
-        private void InititializeNode(HierarchyManager manager)
-        {
-            UIInstance = GameObject.Instantiate(
-                manager.HierarchyConfiguration.ItemPrefab,
-                mParentRef == null ? manager.HierarchyContainer.transform : mParentRef.UIInstance.transform);
-            UIInstance.gameObject.SetActive(mUnfold);
-            UIInstance.Initiate(mTransform.name);
-            UIInstance.OnFold += TriggerFold;
-            UIInstance.OnActivate += TriggerActivate;
-        }
-
-        private void TriggerFold()
-        {
-            foreach (var child in mChildren)
-				child.FoldMe();
-        }
-
-        public void FoldMe()
-        {
-            mUnfold = !mUnfold;
-            UIInstance.gameObject.SetActive(mUnfold);
-		}
-
-        public void TriggerActivate()
-        {
-            if (mTransform.TryGetComponent<ActivationComponent>(out var comp))
-                comp.OnActivate?.Invoke();
-        }
-
-		// TODO : this
-		private void UpdateLocation(int rowIndex)
+		~TreeViewNode()
 		{
-			mRowIndex = rowIndex;
-
-			Vector3 pos = UIInstance.transform.position;
-			pos.x = 795 + rowIndex * mBtnSize;
-			pos.y = 670 - rowIndex * 2 * mBtnSize;
-			pos.z = 0;
-
-			UIInstance.transform.position = pos;
+			Dispose(false);
 		}
 
-        private void Dispose(bool disposing)
+		public void Dispose()
+		{
+			Dispose(disposing: true);
+			GC.SuppressFinalize(this);
+		}
+
+		private void Dispose(bool disposing)
 		{
 			if (!disposedValue)
 			{
 				if (disposing)
 				{
+					mUIInstance.OnFold -= TriggerFold;
+					mUIInstance.OnActivate -= TriggerActivate;
 					GameObject.Destroy(mTransform.gameObject);
 					foreach (var child in mChildren)
 						child.Dispose();
@@ -114,11 +75,62 @@ namespace VENTUS.UnityHierarchyView
 			}
 		}
 
-		public void Dispose()
-		{
-			// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-			Dispose(disposing: true);
-			GC.SuppressFinalize(this);
+		#endregion
+
+		#region private methods
+
+		private void InitializeNode()
+        {
+			mUIInstance = GameObject.Instantiate(
+                mMgrRef.NodePrefab,
+                mParentRef == null 
+					? mMgrRef.HierarchyContainer.transform 
+					: mParentRef.mUIInstance.transform
+			);
+			mUIInstance.gameObject.SetActive(mUnfold);
+			mUIInstance.Initiate(mTransform.name);
+			mUIInstance.OnFold += TriggerFold;
+			mUIInstance.OnActivate += TriggerActivate;
+
+			{   // TODO : update this, so that it is modular
+				Vector3 pos = mUIInstance.transform.position;
+				pos.x = 795 + mDepth * 25;
+				pos.y = 670 - mDepth * 2 * 25;
+				pos.z = 0;
+
+				mUIInstance.transform.position = pos;
+			}
 		}
+
+		private void InitializeChildren()
+        {
+			int childCount = mTransform.childCount;
+			for (int i = 0; i < childCount; i++)
+			{
+				TreeViewNode child = new(mMgrRef, mTransform.GetChild(i), this, mDepth + 1);
+				mChildren.Add(child);
+			}
+		}
+
+        private void TriggerFold()
+        {
+			mUnfold = !mUnfold;
+			foreach (TreeViewNode child in mChildren)
+				child.FoldMe(mUnfold);
+			mUIInstance.OnFolded(mUnfold);
+        }
+
+        public void FoldMe(bool unfold)
+        {
+			mUIInstance.gameObject.SetActive(unfold);
+		}
+
+        private void TriggerActivate()
+        {
+            if (mTransform.TryGetComponent<HierarchyViewActivatable>(out var comp))
+                comp.OnActivate?.Invoke();
+        }
+
+		#endregion
 	}
 }
